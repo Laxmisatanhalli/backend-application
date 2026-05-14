79cc40060ed6d5f6a211d6c97d13b1b5d01d9f59
Laxmi Satanhalli <lsatenhalli@gmail.com>
2026-05-14 18:23:14 +0530

    Added create post and get posts API with image upload

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-   await mongoose.connect("mongodb://yt:Lucky1234@ac-1pwcfhc-shard-00-00.ps50ikk.mongodb.net:27017,ac-1pwcfhc-shard-00-01.ps50ikk.mongodb.net:27017,ac-1pwcfhc-shard-00-02.ps50ikk.mongodb.net:27017/?ssl=true&replicaSet=atlas-cfg1gv-shard-0&authSource=admin&appName=backend")
+   await mongoose.connect(process.env.MONGODB_URI)
     
    console.log("Connected to MongoDB");
 }
